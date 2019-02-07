448f10e6d76d382e0b4078e66f4c833f93aaf87f
Stephen Oliver <skoliver89@gmail.com>
2019-02-07 13:26:21 -0800

    Start using functions in element

--- jsx_intro/src/index.js
+++ jsx_intro/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name = 'Stephen Oliver';
-const element = <h1>Hello, {name}</h1>
+// const name = 'Stephen Oliver';
+// const element = <h1>Hello, {name}</h1>
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Stephen',
+    lastName: 'Oliver'
+};
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+);
 
 ReactDOM.render(
     element,
