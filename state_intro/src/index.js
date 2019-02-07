0c7105d685d7a1ff3dc0504bd8f32dd87ff911b6
Stephen Oliver <skoliver89@gmail.com>
2019-02-07 14:13:11 -0800

    Change clock function to an es6 class; add DOM renderer

--- state_intro/src/index.js
+++ state_intro/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Clock(props) {
-    return (
-        <div>
-            <h1>Hello, World</h1>
-            <h2>It is: {props.date.toLocaleTimeString()}</h2>
-        </div>
-    );
+class Clock extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Hello, World</h1>
+                <h2>It is: {this.props.date.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
+    
 }
+
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+);
 
 //Tutorial Link: https://reactjs.org/docs/state-and-lifecycle.html
