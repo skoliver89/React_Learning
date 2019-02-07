9e927522370d18d02a574e8fe577479638c001b6
Stephen Oliver <skoliver89@gmail.com>
2019-02-07 14:08:52 -0800

    Add component for a clock display

--- state_intro/src/index.js
+++ state_intro/src/index.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function Clock(props) {
+    return (
+        <div>
+            <h1>Hello, World</h1>
+            <h2>It is: {props.date.toLocaleTimeString()}</h2>
+        </div>
+    );
+}
+
+//Tutorial Link: https://reactjs.org/docs/state-and-lifecycle.html
