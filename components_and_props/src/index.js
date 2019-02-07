56130a3be437ffde168edd20d42d06ef78dbb146
Stephen Oliver <skoliver89@gmail.com>
2019-02-07 13:52:04 -0800

    Add the simplest component and render it

--- components_and_props/src/index.js
+++ components_and_props/src/index.js
@@ -1,3 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// The simplest component - In a JS function (Function Component)
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+}
+
+// Can also be defined with an ES6 class (They have additional features that will be discussed later!):
+// class Welcome extends React.Component {
+//     render() {
+//       return <h1>Hello, {this.props.name}</h1>;
+//     }
+//   }
+
+// Rendering a Component
+const element = <Welcome name="Stephen" />;
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
+
+
+//Tutorial Link: https://reactjs.org/docs/components-and-props.html
