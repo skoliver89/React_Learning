2eafbff016e4178cf9b68ed1b7f94adc69483bff
Stephen Oliver <skoliver89@gmail.com>
2019-02-07 14:16:20 -0800

    Add state to the clock class

--- state_intro/src/index.js
+++ state_intro/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Displays a neat clock that uses state in an es6 class
 class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()}
+    }
+
     render(){
         return (
             <div>
                 <h1>Hello, World</h1>
-                <h2>It is: {this.props.date.toLocaleTimeString()}</h2>
+                <h2>It is: {this.state.date.toLocaleTimeString()}</h2>
             </div>
         );
-    }
-    
+    }   
 }
 
 ReactDOM.render(
