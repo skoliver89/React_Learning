0bd0bf46dcfa46259e30a836e43072803a0295a6
Stephen Oliver <skoliver89@gmail.com>
2019-02-07 15:34:17 -0800

    Add a conditional greeting and render it

--- conditional_rendering/src/index.js
+++ conditional_rendering/src/index.js
@@ -1,3 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function UserGreeting(props) {
+    return <h1>Welcome Back!</h1>;
+}
+
+function GuestGreeting(props) {
+    return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    // Conditional rendering works the same way as JS conditionals
+    if (isLoggedIn) {
+        return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+}
+
+ReactDOM.render(
+    // Changing isLoggedIn's value will change the greeting displayed.
+    <Greeting isLoggedIn={false} />,
+    document.getElementById('root')
+);
