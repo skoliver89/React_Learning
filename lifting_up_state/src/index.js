45244689d9e355ad3528574f00b9353bb9ee104c
Stephen Oliver <skoliver89@gmail.com>
2019-02-08 09:50:16 -0800

    Lift state up to parent calculator component

--- lifting_up_state/src/index.js
+++ lifting_up_state/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+};
+
 function BoilingVerdict(props) {
     if (props.celsius){
         if(props.celsius >= 100) {
@@ -11,28 +16,87 @@ function BoilingVerdict(props) {
     return null;
 }
 
-class Calculator extends React.Component {
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
+
+class TemperatureInput extends React.Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
-        this.state = {temperature: ''};
+        //this.state = {temperature: ''};
     }
 
     handleChange(e) {
-        this.setState({temperature: e.target.value});
+        //this.setState({temperature: e.target.value});
+        this.props.onTemperatureChange(e.target.value);
     }
 
     render () {
-        const temperature = this.state.temperature;
+        //const temperature = this.state.temperature;
+        const temperature = this.props.temperature;
+        const scale = this.props.scale;
         return (
             <fieldset>
-                <legend>Enter temperature in Celsius: </legend>
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
                 <input
                     value={temperature}
-                    onChange={this.handleChange} />
-
-                <BoilingVerdict celsius = {parseFloat(temperature)} />             
+                    onChange={this.handleChange} />           
             </fieldset>
+        );
+    }
+}
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+        this.state = {temperature: '', scale: 'c'};
+    }
+
+    handleCelsiusChange(temperature) {
+        this.setState({scale: 'c', temperature});
+    }
+
+    handleFahrenheitChange(temperature) {
+        this.setState({scale: 'f', temperature});
+    }
+
+    render () {
+        const scale = this.state.scale;
+        const temperature = this.state.temperature;
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
+        return (
+            <div>
+                <TemperatureInput 
+                    scale="c"
+                    temperature={celsius}
+                    onTemperatureChange={this.handleCelsiusChange} />
+
+                <TemperatureInput 
+                    scale="f"
+                    temperature={fahrenheit}
+                    onTemperatureChange={this.handleFahrenheitChange} />
+
+                <BoilingVerdict celsius = {parseFloat(celsius)} />
+            </div>
         );
     }
 }
@@ -40,3 +104,5 @@ class Calculator extends React.Component {
 // ====================================================================================
 
 ReactDOM.render(<Calculator />, document.getElementById('root'));
+
+//Tutorial Link: https://reactjs.org/docs/lifting-state-up.html
