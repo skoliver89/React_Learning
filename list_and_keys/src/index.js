d572d23ff682430669ed6420ac505d4f7f70826b
Stephen Oliver <skoliver89@gmail.com>
2019-02-07 16:20:18 -0800

    Render a list of numbers

--- list_and_keys/src/index.js
+++ list_and_keys/src/index.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number) =>
+    <li>{number}</li>
+);
+
+ReactDOM.render(
+    <ul>{listItems}</ul>,
+    document.getElementById('root')
+);
