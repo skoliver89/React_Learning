034b8bb537994debb5adee08a560b5e7f8849aca
Stephen Oliver <skoliver89@gmail.com>
2019-02-07 16:23:35 -0800

    render the numbers inside of a component

--- list_and_keys/src/index.js
+++ list_and_keys/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const numbers = [1, 2, 3, 4, 5];
-const listItems = numbers.map((number) =>
-    <li>{number}</li>
-);
+function NumberList(props) {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number) =>
+        <li>{number}</li>
+    );
+    return (
+        <ul>{listItems}</ul>
+    );
+}
 
+const numbers = [1, 2, 3, 4, 5];
 ReactDOM.render(
-    <ul>{listItems}</ul>,
+    <NumberList numbers={numbers} />,
     document.getElementById('root')
 );
