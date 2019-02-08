4243c3314720e31fa8132f0c604c95e41c3886a7
Stephen Oliver <skoliver89@gmail.com>
2019-02-08 10:28:35 -0800

    missing semi colon on lifting up state; init learning composition vs inheritance

---
+++ composition_v_inheritance/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// =================================================================
+
+
+// Tutorial Link: https://reactjs.org/docs/composition-vs-inheritance.html
+// Recommended to use composition rather than inheritance 
+// to reuse code between components
