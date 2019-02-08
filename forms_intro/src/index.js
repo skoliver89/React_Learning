754e04c2d5faafdb19260c53f0272471fffe8085
Stephen Oliver <skoliver89@gmail.com>
2019-02-07 19:02:36 -0800

    Added flavor form with a selector element

--- forms_intro/src/index.js
+++ forms_intro/src/index.js
@@ -7,6 +7,8 @@ function App() {
             <NameForm />
             <br />
             <EssayForm />
+            <br />
+            <FlavorForm />
         </div>
     );
 }
@@ -77,7 +79,46 @@ class EssayForm extends React.Component {
     }
 }
 
+class FlavorForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: 'coconut'};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('Your favorite flavor is: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render () {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Pick your favorite flavor: 
+                    <select value={this.state.value}
+                    onChange={this.handleChange}>
+                        <option value="grapefruit">Grapefruit</option>
+                        <option value="lime">Lime</option>
+                        <option value="coconut">Coconut</option>
+                        <option value="mango">Mango</option>
+                    </select>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
+
 ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
+
+// Tutorial Link: https://reactjs.org/docs/forms.html
