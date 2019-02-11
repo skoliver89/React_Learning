8af7c4e84d0cf63427d23043252b52d64880d90f
Stephen Oliver <skoliver89@gmail.com>
2019-02-11 13:23:41 -0800

    Added a little testing using JEST

--- PluralSight_Tutorial/author_quiz/src/authorQuiz.test.js
+++ PluralSight_Tutorial/author_quiz/src/authorQuiz.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AuthorQuiz from './authorQuiz.js';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("Author Quiz", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<AuthorQuiz />, div);
+  });
 });
