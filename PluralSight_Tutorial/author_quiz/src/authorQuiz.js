a0905f9560214b02fc53ac4540be37735c8b1aa3
Stephen Oliver <skoliver89@gmail.com>
2019-02-11 16:32:36 -0800

    Finished Module: Events

--- PluralSight_Tutorial/author_quiz/src/authorQuiz.js
+++ PluralSight_Tutorial/author_quiz/src/authorQuiz.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './authorQuiz.css';
 import './bootstrap.min.css'
 
-function AuthorQuiz ({turnData}) {
+function AuthorQuiz ({turnData, highlight, onAnswerSelected}) {
 
     return (
       <div className="container-fluid">
         <Hero />
-        <Turn {...turnData}/>
+        <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected}/>
         <Continue />
         <Footer />
       </div>
@@ -25,22 +25,31 @@ function Hero() {
   );
 }
 
-function Book({title}) {
+function Book({title, onClick}) {
   return(
-    <div className="answer">
+    <div className="answer" onClick={() => {onClick(title);}}>
       <h4>{title}</h4>
     </div>
   );
 }
 
-function Turn({author, books}) {
+function Turn({author, books, highlight, onAnswerSelected}) {
+  function highlightToBgColor(highlight){
+    const mapping ={
+      'none' : '',
+      'correct' : 'green',
+      'wrong' : 'red'
+    };
+    return mapping[highlight];
+  }
+
   return(
-    <div className="row turn" style={{backgroundColor: "white"}}>
+    <div className="row turn" style={{backgroundColor: highlightToBgColor(highlight)}}>
       <div className="col-4 offset-1">
         <img src={author.imageUrl} className="authorImage" alt="Author" />
       </div>
       <div className="col-6">
-        {books.map((title) => <Book title={title} ket={title}/>)}
+        {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected} />)}
       </div>
     </div>
   );
--- PluralSight_Tutorial/author_quiz/src/index.js
+++ PluralSight_Tutorial/author_quiz/src/index.js
@@ -61,11 +61,21 @@ function getTurnData(authors) {
     }
 }
 
+function onAnswerSelected(answer) {
+    const isCorrect = state.turnData.author.books.some((book) => book === answer);
+    state.highlight = isCorrect ? 'correct' : 'wrong';
+    render();
+}
+
 const state = {
-    turnData: getTurnData(authors)
+    turnData: getTurnData(authors),
+    highlight: 'none'
 };
 
-ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById('root'));
+function render() {
+    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />, document.getElementById('root'));
+}
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
