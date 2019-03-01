71907b4c7a67b2908a91b1dd7c7fef799c58429e
Stephen Oliver <skoliver89@gmail.com>
2019-03-01 11:59:51 -0800

    Disable linting on ManageCoursePage import

--- PluralSight_Tutorials/Path_Intermediate/Apps_With_React_And_Redux/App/src/routes.js
+++ PluralSight_Tutorials/Path_Intermediate/Apps_With_React_And_Redux/App/src/routes.js
@@ -4,6 +4,7 @@ import App from "./components/App";
 import HomePage from "./components/home/HomePage";
 import AboutPage from "./components/about/AboutPage";
 import CoursesPage from "./components/course/CoursesPage";
+// eslint-disable-next-line import/no-named-as-default
 import ManageCoursePage from "./components/course/ManageCoursePage";
 
 export default (
