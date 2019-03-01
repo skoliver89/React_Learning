3cf6184d064e681d9e0b4648e3c9c5d9c3b0a884
Stephen Oliver <skoliver89@gmail.com>
2019-03-01 12:03:00 -0800

    export class so can test raw component; extract author selector formatting function

--- PluralSight_Tutorials/Path_Intermediate/Apps_With_React_And_Redux/App/src/components/course/ManageCoursePage.js
+++ PluralSight_Tutorials/Path_Intermediate/Apps_With_React_And_Redux/App/src/components/course/ManageCoursePage.js
@@ -4,8 +4,9 @@ import { bindActionCreators } from "redux";
 import toastr from "toastr";
 import * as courseActions from "../../actions/courseActions";
 import CourseForm from "./CourseForm";
+import { authorsFormattedForDropdown } from "../../selectors/AuthorSelectors";
 
-class ManageCoursePage extends Component {
+export class ManageCoursePage extends Component {
   constructor(props, context) {
     super(props, context);
 
@@ -34,8 +35,26 @@ class ManageCoursePage extends Component {
     return this.setState({ course: course });
   }
 
+  courseFormIsValid() {
+    let formISValid = true;
+    let errors = {};
+
+    if (this.state.course.title.length < 5) {
+      errors.title = "Title must be at least 5 characters.";
+      formISValid = false;
+    }
+
+    this.setState({ errors: errors });
+    return formISValid;
+  }
+
   onSaveClick(event) {
     event.preventDefault();
+
+    if (!this.courseFormIsValid()) {
+      return;
+    }
+
     this.setState({ saving: true });
     this.props.actions
       .saveCourse(this.state.course)
@@ -101,16 +120,9 @@ function mapStateToProps(state, ownProps) {
     course = getCourseById(state.courses, courseId);
   }
 
-  const authorsFormattedForDropdown = state.authors.map(author => {
-    return {
-      value: author.id,
-      text: author.firstName + " " + author.lastName
-    };
-  });
-
   return {
     course: course,
-    authors: authorsFormattedForDropdown
+    authors: authorsFormattedForDropdown(state.authors)
   };
 }
 
